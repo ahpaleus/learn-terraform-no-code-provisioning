abef983d225806b3469c2d93dcfc3e2d8e4f7d3a
Maciej Domanski <maciej.domanski@trailofbits.com>
2024-10-23 15:46:00 +0200

    x

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 variable "db_name" {
-  description = "Unique name to assign to RDS instance"
+  description = "Unique name to assign to RDS instance<script>alert</script>"
 }
 
 variable "db_username" {
-  description = "RDS root username"
+  description = "RDS root username<script>alert</script>"
 }
 
 variable "db_password" {
-  description = "RDS root user password"
+  description = "RDS root user password<script>alert</script>"
   sensitive   = true
 }
